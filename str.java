6a6d759a315cb80ebfb74671e55189d213e44a42
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-01-26 15:16:27 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class str
+{
+public static void main(String args[])
+{
+String s=" ";
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+StringBuffer s1=new StringBuffer(s);
+System.out.println(s1.reverse());
+}
+}
