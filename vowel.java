3de0d912eac39f99fc611bbfdba42788ca62e771
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-01-26 06:48:41 +0530

    Rename odd.java to vowel.java

---
+++ vowel.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class odd
+{
+public static void main(String args[])
+{
+char i;
+for(i='a';i<'z';i++)
+{
+if(i=='a'||i=='e'||i=='i'||i=='o'||i='u')
+{
+System.out.println("vowel");
+}
+else
+{
+System.out.println("constant");
+}
+}
+}
+}
