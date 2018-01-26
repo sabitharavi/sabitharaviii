4f461a97e060fbfb45d6f4b52b3f8128ed9e1c5e
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-01-26 06:59:19 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class alpha
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char i=sc.next();
+if((i>='a'&&i<='z')||(i>='A'&&i<='Z'))
+{
+System.out.println("alphabet");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
