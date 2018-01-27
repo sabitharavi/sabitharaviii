4ee207d85e7a73866b5aad6d4012d3434f9b2351
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-01-27 17:01:16 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class app
+{
+public static void main(String args[])
+{
+String stl=" ";
+Scanner sc=new Scanner(System.in);
+stl=sc.nextLine();
+StringBuffer ss=new StringBuffer(stl);
+System.out.println(ss.append("."));
+}
+}
