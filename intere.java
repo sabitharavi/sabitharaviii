c17cd775d0831ad657e8a1313e09eabf62f913f2
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-02-01 20:16:01 +0530

    Create intere.java

---
+++ intere.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class intere
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        Scanner sc=new Scanner(System.in);
+        i=sc.nextInt();
+        j=sc.nextInt();
+        for(int k=i;k<=j;k++)
+        {
+            if(k%2==0)
+            {
+                System.out.print(k+" ");
+            }
+        }
+    }
+}
