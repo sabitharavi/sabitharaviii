a10d22ac52244edc2e0f444fb57ecc665eb5bc00
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-02-18 13:18:53 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class substring
+{
+    public static void main(String args[])
+    {
+        String ar=" ";
+        String arr=" ";
+        Scanner sc=new Scanner(System.in);
+        ar=sc.nextLine();
+        arr=sc.nextLine();
+         System.out.print(ar.contains(arr));
+    }
+}
