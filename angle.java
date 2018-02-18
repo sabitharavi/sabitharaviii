05e0d8449c96fc221f254cfc1ca4554d6513aaf8
sabitharavi <34501300+sabitharavi@users.noreply.github.com>
2018-02-18 13:37:25 +0530

    Create angle.java

---
+++ angle.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class angle
+{
+    public static void main(String args[])
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.print(Math.sin(a));
+    }
+}
